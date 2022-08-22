7553224472b5ff4d95ab82eaf71aadf8c2c73218
NguyenTrongTin <ntrongtin11702@gmail.com>
2022-08-22 14:59:21 +0700

    add rep-run domain

--- src/cors.ts
+++ src/cors.ts
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 const options: cors.CorsOptions = {
-  origin: ["http://localhost:3000"]
+  origin: ["http://localhost:3000", "https://rep-run-ui.vercel.app/"]
 }
 
 export const CORS = cors(options);
