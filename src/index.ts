842d3096b06c7e16594892c755c4d64af823be7a
ducle <ducle@chidoanh.com>
2022-09-01 21:56:17 +0700

    add any data API

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import express from 'express';
-import {IPFSUtils} from './IPFSUtils';
-import {CORS} from './cors';
-import multer from 'multer';
+import express from "express";
+import { IPFSUtils } from "./IPFSUtils";
+import { CORS } from "./cors";
+import multer from "multer";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 8000;
@@ -10,31 +11,50 @@ const upload = multer();
 
 app.use(CORS);
 // app.use(express.json())
-// app.use(upload.array('files')); 
-app.use(express.static('public'));
+// app.use(upload.array('files'));
+app.use(express.static("public"));
 
-app.post("/uploadFile",
-  upload.single('file'),
-  (req, res) => {
-    console.log("REQ", req.files)
+app.post("/uploadFile", upload.single("file"), (req, res) => {
+  console.log("REQ", req.files);
 
-    if (!req.file) return;
-    IPFSUtils.uploadArrayBufferToIPFS({
-      buffer: req.file.buffer,
-      onSuccess: (url: string) => {
-        res.send({"hash": url})
-      },
-      onError: (e: any) => {
-        console.error(e);
-        res.send(e);
-      }
-    })
-  })
+  if (!req.file) {
+    res.status(400).send("Empty upload data");
+    return;
+  }
+  IPFSUtils.uploadArrayBufferToIPFS({
+    buffer: req.file.buffer,
+    onSuccess: (url: string) => {
+      res.send({ hash: url });
+    },
+    onError: (e: any) => {
+      console.error(e);
+      res.send(e);
+    },
+  });
+});
 
-app.get("/", (req ,res) => res.send("IPFS Server"));
+app.post("/upload", bodyParser.json(), (req, res) => {
+  if (!req.body) {
+    res.status(400).send("Empty upload data");
+    return;
+  }
+
+  IPFSUtils.uploadArrayBufferToIPFS({
+    buffer: req.body.data,
+    onSuccess: (url: string) => {
+      res.send({ hash: url });
+    },
+    onError: (e: any) => {
+      console.error(e);
+      res.send(e);
+    },
+  });
+});
+
+app.get("/", (req, res) => res.send("IPFS Server"));
 
 app.listen(port, () => {
   console.log("Start listening to port ", port);
-})
+});
 
 module.exports = app;
